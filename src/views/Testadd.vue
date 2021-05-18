8517afeb436692e90789d727a0b19876278247f0
xm-shisilang <1977167246@qq.com>
2021-05-18 23:18:48 +0800

    create new vue file Testadd.vue

---
+++ src/views/Testadd.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="testadd">
+    <ul>
+      <li>{{ movies[0] }}</li>
+      <li>{{ movies[1] }}</li>
+      <li>{{ movies[2] }}</li>
+      <li>{{ movies[3] }}</li>
+      <li>{{ movies[4] }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Testadd",
+  data() {
+    return {
+      movies: ["海王", "海贼王", "美人鱼", "加勒比海盗", "进击的巨人"],
+    };
+  },
+};
+</script>
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
