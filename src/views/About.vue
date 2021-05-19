48426c4012cdf85752a3ac99bd667a6559887562
xm-shisilang <1977167246@qq.com>
2021-05-19 19:18:00 +0800

    纠正合并操作代码

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,15 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h2>test git push , step = 1</h2>
-    <h2>git checkout main 切换到原分支main ，原分支 main 中内容</h2>
+    <h2>
+      原main 的冲突内容（main 内第5行），git checkout main 切换到原分支main
+      ，原分支 main 中内容
+    </h2>
     <h2>这是 main 分支 的内容，测试 分支技术</h2>
     <h2>test branch , main / dev</h2>
-    <h2>现在已经不能 修改 原分支 main 中内容</h2>
+    <h2>
+      原Dev 的冲突内容（dev 内第5行），现在已经不能 修改 原分支 main 中内容
+    </h2>
     <h2>这是 main 分支 的内容</h2>
     <h2>test push to div</h2>
   </div>
