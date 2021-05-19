4f0be6c2c6f13980bbc47cbedd5e4e5a443fb206
xm-shisilang <1977167246@qq.com>
2021-05-19 18:44:52 +0800

    git commit -m -a

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h2>test git push , step = 1</h2>
-    <h2>修改 原分支 main 中相同位置</h2>
+    <h2>现在已经不能 修改 原分支 main 中内容</h2>
     <h2>这是 main 分支 的内容</h2>
   </div>
 </template>
