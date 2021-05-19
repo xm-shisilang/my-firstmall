76a321ba5e9793ad0788566299e4d9be3c5adcc7
xm-shisilang <1977167246@qq.com>
2021-05-19 22:39:04 +0800

    clonemall changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <div class="clonemall">
+      <h2>这是在clone-myfirstmall 下进行的修改</h2>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style scoped>
+.clonemall {
+  background-color: skyblue;
+}
+.clonemall h2 {
+  color: red;
+}
+</style>
