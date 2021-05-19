88747fae76491375f10a94cac6e7251dd628bb43
xm-shisilang <1977167246@qq.com>
2021-05-19 18:24:14 +0800

    -a

--- src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h2>test git push , step = 1</h2>
-    <h2>修改 原分支 main 中相同位置</h2>
-    <h2>这是 main 分支 的内容</h2>
-  </div>
-</template>
